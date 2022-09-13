3686a88d4e18f962c80937c3002f10c35b481d7d
wolfjfly <104739255+wolfjfly@users.noreply.github.com>
2022-09-13 17:07:13 -0500

    adding first popup window

--- assets/script.js
+++ assets/script.js
@@ -2,13 +2,14 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+function writePassword() { confirmgit -A("So you want to create a password");
+  // var password = generatePassword();
+  // var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  // passwordText.value = password;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+ 
